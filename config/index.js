44e867d8517a9ae6452a75447c04abff9555f805
Robert Kirsz <robert.kirsz@gmail.com>
2016-10-21 03:24:32 +0200

    Installed the newest version of react-redux-starter-kit

--- config/index.js
+++ config/index.js
@@ -1,29 +1,131 @@
-import fs from 'fs'
-import _debug from 'debug'
-import config from './_base'
+/* eslint key-spacing:0 spaced-comment:0 */
+const path = require('path')
+const debug = require('debug')('app:config')
+const argv = require('yargs').argv
+const ip = require('ip')
 
-const debug = _debug('app:config')
-debug('Create configuration.')
-debug(`Apply environment overrides for NODE_ENV "${config.env}".`)
+debug('Creating default configuration.')
+// ========================================================
+// Default Configuration
+// ========================================================
+const config = {
+  env : process.env.NODE_ENV || 'development',
 
-// Check if the file exists before attempting to require it, this
-// way we can provide better error reporting that overrides
-// weren't applied simply because the file didn't exist.
-const overridesFilename = `_${config.env}`
-let hasOverridesFile
-try {
-  fs.lstatSync(`${__dirname}/${overridesFilename}.js`)
-  hasOverridesFile = true
-} catch (e) {}
+  // ----------------------------------
+  // Project Structure
+  // ----------------------------------
+  path_base  : path.resolve(__dirname, '..'),
+  dir_client : 'src',
+  dir_dist   : 'dist',
+  dir_server : 'server',
+  dir_test   : 'tests',
 
-// Overrides file exists, so we can attempt to require it.
-// We intentionally don't wrap this in a try/catch as we want
-// the Node process to exit if an error occurs.
-let overrides
-if (hasOverridesFile) {
-  overrides = require(`./${overridesFilename}`).default(config)
-} else {
-  debug(`No configuration overrides found for NODE_ENV "${config.env}"`)
+  // ----------------------------------
+  // Server Configuration
+  // ----------------------------------
+  server_host : ip.address(), // use string 'localhost' to prevent exposure on local network
+  server_port : process.env.PORT || 3000,
+
+  // ----------------------------------
+  // Compiler Configuration
+  // ----------------------------------
+  compiler_babel : {
+    cacheDirectory : true,
+    plugins        : ['transform-runtime'],
+    presets        : ['es2015', 'react', 'stage-0']
+  },
+  compiler_devtool         : 'source-map',
+  compiler_hash_type       : 'hash',
+  compiler_fail_on_warning : false,
+  compiler_quiet           : false,
+  compiler_public_path     : '/',
+  compiler_stats           : {
+    chunks : false,
+    chunkModules : false,
+    colors : true
+  },
+  compiler_vendors : [
+    'react',
+    'react-redux',
+    'react-router',
+    'redux'
+  ],
+
+  // ----------------------------------
+  // Test Configuration
+  // ----------------------------------
+  coverage_reporters : [
+    { type : 'text-summary' },
+    { type : 'lcov', dir : 'coverage' }
+  ]
 }
 
-export default Object.assign({}, config, overrides)
+/************************************************
+-------------------------------------------------
+
+All Internal Configuration Below
+Edit at Your Own Risk
+
+-------------------------------------------------
+************************************************/
+
+// ------------------------------------
+// Environment
+// ------------------------------------
+// N.B.: globals added here must _also_ be added to .eslintrc
+config.globals = {
+  'process.env'  : {
+    'NODE_ENV' : JSON.stringify(config.env)
+  },
+  'NODE_ENV'     : config.env,
+  '__DEV__'      : config.env === 'development',
+  '__PROD__'     : config.env === 'production',
+  '__TEST__'     : config.env === 'test',
+  '__COVERAGE__' : !argv.watch && config.env === 'test',
+  '__BASENAME__' : JSON.stringify(process.env.BASENAME || '')
+}
+
+// ------------------------------------
+// Validate Vendor Dependencies
+// ------------------------------------
+const pkg = require('../package.json')
+
+config.compiler_vendors = config.compiler_vendors
+  .filter((dep) => {
+    if (pkg.dependencies[dep]) return true
+
+    debug(
+      `Package "${dep}" was not found as an npm dependency in package.json; ` +
+      `it won't be included in the webpack vendor bundle.
+       Consider removing it from \`compiler_vendors\` in ~/config/index.js`
+    )
+  })
+
+// ------------------------------------
+// Utilities
+// ------------------------------------
+function base () {
+  const args = [config.path_base].concat([].slice.call(arguments))
+  return path.resolve.apply(path, args)
+}
+
+config.utils_paths = {
+  base   : base,
+  client : base.bind(null, config.dir_client),
+  dist   : base.bind(null, config.dir_dist)
+}
+
+// ========================================================
+// Environment Configuration
+// ========================================================
+debug(`Looking for environment overrides for NODE_ENV "${config.env}".`)
+const environments = require('./environments')
+const overrides = environments[config.env]
+if (overrides) {
+  debug('Found overrides, applying to default configuration.')
+  Object.assign(config, overrides(config))
+} else {
+  debug('No environment overrides found, defaults will be used.')
+}
+
+module.exports = config
---
+++ src-backup/views/MainView.js
@@ -0,0 +1,292 @@
+import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { actions as collectionActions } from 'redux/modules/collection'
+import { actions as modalActions } from 'redux/modules/modal'
+import { actions as saveAjaxActions } from 'redux/modules/saveCardsFromAjax'
+import { actions as decksActions } from 'redux/modules/decks'
+import { actions as applicationActions } from 'redux/modules/application'
+import CoolCardsDisplay from 'components/CoolCardsDisplay'
+import CardsList from 'components/CardsList'
+import CardNamesList from 'components/CardNamesList'
+import DecksPanel from 'components/DecksPanel'
+import Search from 'components/Search'
+import Modal from 'components/Modal'
+import _ from 'lodash'
+import Card from 'models/Card'
+import CollectionStats from 'components/CollectionStats'
+
+import databaseMaker from 'database/databaseMaker'
+
+// TODO - czyścić stan wyszukiwania pomiędzy przelączaniem Searcha
+
+const $ = window.jQuery
+
+const mapStateToProps = (state) => ({
+  application: state.application,
+  cardsCollectionFromStore: state.collection,
+  database: state.database,
+  modal: state.modal,
+  decks: state.decks
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  collectionActions: bindActionCreators(collectionActions, dispatch),
+  modalActions: bindActionCreators(modalActions, dispatch),
+  saveAjaxActions: bindActionCreators(saveAjaxActions, dispatch),
+  decksActions: bindActionCreators(decksActions, dispatch),
+  applicationActions: bindActionCreators(applicationActions, dispatch)
+})
+
+class MainView extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      outputStyle: 'text',
+      cardsToDisplay: 50,
+      cardArtworks: [],
+      databaseStatus: 'downloading',
+      searchResults: [],
+      searchCollectionResults: [],
+      showSearchDatabasePanel: false, // Shows search database panel when true
+      showSearchCollectionPanel: false, // Shows search collection panel when true
+      showDecksPanel: false, // Shows search collection panel when true
+      showCoolCards: true,
+      // ---------- new state
+      magicCards: null,
+      latestSet: null,
+      setsNumber: null,
+      uniqueCardsNumber: null
+    }
+    this._addCardToCollection = this._addCardToCollection.bind(this)
+    this._removeCardFromCollection = this._removeCardFromCollection.bind(this)
+    this._searchInSets = this._searchInSets.bind(this)
+    this._showMoreCards = this._showMoreCards.bind(this)
+    this._updateCardNamesList = this._updateCardNamesList.bind(this)
+    this._openModal = this._openModal.bind(this)
+    this._filterCollection = this._filterCollection.bind(this)
+    this._addCardToDeck = this._addCardToDeck.bind(this)
+  }
+
+  static propTypes = {
+    application: React.PropTypes.object,
+    cardsCollectionFromStore: React.PropTypes.array,
+    database: React.PropTypes.object,
+    collectionActions: React.PropTypes.object,
+    modalActions: React.PropTypes.object,
+    saveAjaxActions: React.PropTypes.object,
+    decksActions: React.PropTypes.object,
+    applicationActions: React.PropTypes.object,
+    modal: React.PropTypes.object,
+    decks: React.PropTypes.array
+  }
+
+  componentWillMount () {
+    this.getCardsFromTheServer()
+  }
+
+  componentWillReceiveProps (nextProps) {
+    // Get sample from a collection when it arives
+    if (
+      this.props.cardsCollectionFromStore.length === 0 &&
+      nextProps.cardsCollectionFromStore.length !== 0 &&
+      this.state.searchCollectionResults.length === 0
+    ) {
+      this.setState({ searchCollectionResults: _.sampleSize(nextProps.cardsCollectionFromStore, 26) })
+    }
+  }
+
+  async getCardsFromTheServer () {
+    console.log('⬇️ Downloading started')
+    const magicDatabase = await databaseMaker()
+    if (magicDatabase) {
+      console.log('👍 Downloading completed!')
+      this.setState({...magicDatabase}) // 'magicCards', 'latestSet', 'setsNumber', 'uniqueCardsNumber'
+      // this.loadLocalStorage()
+    }
+  }
+
+  loadLocalStorage () {
+    // Retrieve card data from local Storage
+    const retrievedReducedCollection = JSON.parse(localStorage.getItem('mtgdbReducedCollectionRK'))
+    // For each card from the 'retrievedCollection'...
+    const transformedRetrievedReducedCollection = _.map(
+      retrievedReducedCollection,
+      (retrievedCard) => {
+        // For each card set...
+        for (let i = 0; i < allSets.length; i++) {
+          // And for each card in each set...
+          for (let j = 0; j < allSets[i].cards.length; j++) {
+            // If that card's id is the same as the id of the retrieved card...
+            if (allSets[i].cards[j].id === retrievedCard.id) {
+              // Return it
+              return new Card({ ...allSets[i].cards[j], ...retrievedCard })
+            }
+          }
+        }
+      }
+    )
+    // Save retrievedCollection in the store
+    if (transformedRetrievedReducedCollection.length) {
+      this.props.collectionActions.restoreCollection(transformedRetrievedReducedCollection)
+    }
+  }
+
+  _openModal (cardToDisplay) {
+    this.props.modalActions.openModal({ modalName: 'card-details', cardToDisplay })
+  }
+
+  _addCardToCollection (card) {
+    this.props.collectionActions.addCard(card)
+  }
+
+  _removeCardFromCollection (card) {
+    this.props.collectionActions.removeCard(card)
+  }
+
+  _searchInSets (card) {
+    // console.log('You clicked on a ' + card.name, card)
+    let cards = []
+    const allSets = this.props.database.allSets
+    // For each card set...
+    for (let i = 0; i < allSets.length; i++) {
+      // And for each card in each set...
+      for (let j = 0; j < allSets[i].cards.length; j++) {
+        // If that card's name is the same as clicked card name and it has a 'multiverseid'...
+        if (allSets[i].cards[j].name === card.name && allSets[i].cards[j].multiverseid) {
+          // Add that card to an array
+          cards.push({
+            ...allSets[i].cards[j],
+            setIcon: allSets[i].code.toLowerCase()
+          })
+          // console.log('   I found it in ' + allSets[i].name, allSets[i])
+        }
+      }
+    }
+    this.setState({cardArtworks: cards})
+  }
+
+  _showMoreCards () {
+    this.setState({ cardsToDisplay: this.state.cardsToDisplay + 50 })
+  }
+
+  _updateCardNamesList (searchResults = []) {
+    this.setState({ searchResults, cardArtworks: [] })
+  }
+
+  _filterCollection (searchCollectionResults = []) {
+    this.setState({ searchCollectionResults, searchResults: [], cardArtworks: [] })
+  }
+
+  // DECKS
+
+  _addCardToDeck (cardId) {
+    // TODO ten cardId to teraz name. Może zamiast samego name i amount trzymać teżjakieś id?
+    // Add card to the deck
+    this.props.decksActions.addCardToDeck(cardId, this.props.application.activeDeck)
+  }
+
+  render () {
+    const {
+      searchResults, cardArtworks, cardsToDisplay,
+      searchCollectionResults, showDecksPanel, showCoolCards,
+      showSearchDatabasePanel, showSearchCollectionPanel, magicCards
+    } = this.state
+    const { database, cardsCollectionFromStore } = this.props
+    const numberOfCardsInCollection = cardsCollectionFromStore.reduce((a, b) => a + b.cardsInCollection, 0)
+
+    const cardsCollectionPanel = (
+      <div className='cards-collection-panel'>
+        <h3>Collection ({numberOfCardsInCollection})</h3>
+        {/* <CollectionStats /> */}
+        {
+          showCoolCards ?
+            <CoolCardsDisplay
+              cards={searchCollectionResults}
+              onClick={() => { this.setState({ showCoolCards: false }) }}
+            />
+            :
+            <CardsList
+              cards={searchCollectionResults}
+              openModal={this._openModal}
+              addCard={this._addCardToCollection}
+              removeCard={this._removeCardFromCollection}
+              addCardToDeck={this._addCardToDeck}
+            />
+        }
+      </div>
+    )
+    const cardNamesList = (
+      <CardNamesList
+        cards={searchResults.slice(0, cardsToDisplay)}
+        collection={cardsCollectionFromStore}
+        moreCardsToShow={searchResults.length > cardsToDisplay}
+        onCardClick={this._searchInSets}
+        onMoreClick={this._showMoreCards}
+      />
+    )
+    const cardArtworksPanel = (
+      <div className='card-artworks-panel'>
+        <h3>Artworks</h3>
+        <CardsList
+          cards={cardArtworks}
+          openModal={this._openModal}
+          addCard={this._addCardToCollection}
+          removeCard={this._removeCardFromCollection}
+        />
+      </div>
+    )
+    const panelsNavigationButtons = (
+      <div className='panels-navigation-buttons'>
+        <div
+          className={'panels-navigation-buttons__button' + (showSearchCollectionPanel ? ' panels-navigation-buttons__button--active' : '')}
+          onClick={() => { this.setState({ showSearchCollectionPanel: !showSearchCollectionPanel, showSearchDatabasePanel: false }) }}
+        >
+          <i className='icon fa fa-search' />
+        </div>
+        <div
+          className={'panels-navigation-buttons__button' + (showSearchDatabasePanel ? ' panels-navigation-buttons__button--active' : '')}
+          onClick={() => { this.setState({ showSearchDatabasePanel: !showSearchDatabasePanel, showSearchCollectionPanel: false }) }}
+        >
+          <i className='icon fa fa-plus-circle' />
+        </div>
+        <div
+          className={'panels-navigation-buttons__button' + (showDecksPanel ? ' panels-navigation-buttons__button--active' : '')}
+          onClick={() => { this.setState({ showDecksPanel: !showDecksPanel }) }}
+        >
+          <i className='icon fa fa-clone' />
+        </div>
+      </div>
+    )
+    const searchDatabasePanel = (
+      <Search
+        title='Search Database'
+        collectionToSearchIn={magicCards}
+        onSearch={this._updateCardNamesList}
+      />
+    )
+    const searchCollectionPanel = (
+      <Search
+        title='Search Collection'
+        collectionToSearchIn={cardsCollectionFromStore}
+        onSearch={this._filterCollection}
+        allowToFilterWholeCollection
+      />
+    )
+
+    return (
+      <div className='search-in-collection-view'>
+        {this.props.modal ? <Modal /> : null}
+        {showSearchDatabasePanel ? searchDatabasePanel : null}
+        {showSearchCollectionPanel ? searchCollectionPanel : null}
+        {panelsNavigationButtons}
+        {searchResults.length ? cardNamesList : null}
+        {cardArtworks.length ? cardArtworksPanel : null}
+        {cardsCollectionFromStore.length ? cardsCollectionPanel : null}
+        {showDecksPanel ? <DecksPanel /> : null}
+      </div>
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainView)
