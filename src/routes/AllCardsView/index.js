62ba3a33c5c4298d84c01902798104bf2bda18c8
Robert Kirsz <robert.kirsz@gmail.com>
2016-10-21 18:34:38 +0200

    Added route for the "all cards" page

---
+++ src/routes/AllCardsView/index.js
@@ -0,0 +1,6 @@
+import AllCardsView from './views/AllCardsView'
+
+export default {
+  component : AllCardsView,
+  path      : 'all-cards'
+}
---
+++ src/routes/AllCardsView/views/AllCardsView.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './AllCardsView.scss'
+
+export const AllCardsView = () => (
+  <div>
+    <h4>AllCardsView</h4>
+  </div>
+)
+
+export default AllCardsView
