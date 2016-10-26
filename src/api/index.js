2dc7b29a68fcdbde8acb8fbfc13a2fc3b439caa0
Robert Kirsz <robert.kirsz@gmail.com>
2016-10-27 00:12:10 +0200

    Added different API URLs for development and production

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,4 @@
 export const fetchAllSets = () => {
-  // json-server AllSets.json --port 3001
-
-  return fetch('http://localhost:3001/db')
-  // return fetch('https://mtgjson.com/json/AllSets.json')
+  if (__DEV__) return fetch('http://localhost:3001/db') // From 'src/api' -> json-server AllSets.json --port 3001
+  if (__PROD__) return fetch('https://mtgjson.com/json/AllSets.json')
 }
