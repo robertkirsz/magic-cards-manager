19782f0076337be3435ee80d3c53b66d6359b3c5
Robert Kirsz <robert.kirsz@gmail.com>
2016-11-12 18:17:38 +0100

    Updated Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,12 @@ import { IndexLink, Link } from 'react-router'
 import { LatestSet } from 'components'
 
 export const Header = () => (
-  <header className="header">
-    <ul className="nav nav-pills">
-      <li className="nav-item">
-        <IndexLink to="/" className="nav-link" activeClassName="active">Home</IndexLink>
-      </li>
-      <li className="nav-item">
-        <Link to="/cards" className="nav-link" activeClassName="active">All cards</Link>
-      </li>
-    </ul>
+  <header className="main-header">
+    <nav className="route-navigation">
+      <IndexLink to="/" className="nav-link" activeClassName="active">Home</IndexLink>
+      <Link to="/all-cards" className="nav-link" activeClassName="active">All cards</Link>
+      <Link to="/my-cards" className="nav-link" activeClassName="active">My cards</Link>
+    </nav>
     <LatestSet />
   </header>
 )
