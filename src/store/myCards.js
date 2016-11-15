0efda28c4a51c1316b80dbfebc3ebc6000bfa59d
Robert Kirsz <robert.kirsz@gmail.com>
2016-11-15 23:44:38 +0100

    Cards can now be removed from the collection

--- src/store/myCards.js
+++ src/store/myCards.js
@@ -1,5 +1,4 @@
 import _ from 'lodash'
-import Card from 'classes/Card'
 
 const updateLocalStorage = (cardMyCards) => {
   // const reducedMyCards = _.map(cardMyCards, (singleCard) => singleCard.formatForLocalStorage())
@@ -21,9 +20,8 @@ export const addCard = (card, variant) => ({
   type: ADD_CARD, card, variant
 })
 
-export const removeCard = (card) => ({
-  type: REMOVE_CARD,
-  payload: card
+export const removeCard = (card, variant) => ({
+  type: REMOVE_CARD, card, variant
 })
 
 export const clearMyCards = () => ({
@@ -47,29 +45,28 @@ export const actions = {
 // ------------------------------------
 const ACTION_HANDLERS = {
   [ADD_CARD]: (state, { card, variant }) => {
-    console.log('%cADD_CARD', 'color: #A1C659;', variant)
+    console.log('%cADD_CARD', 'color: #A1C659;', variant.name)
     // Make copies of both main card and its chosen variant
     let cardCopy = card.copy()
     const variantCopy = variant.copy()
     // Copy card collection from store
     let cardsCollection = [...state.cards]
     // Check if this card already exists in collection
-    const cardInCollection = _.find(cardsCollection, cardFromCollection => cardFromCollection.name === cardCopy.name)
+    const cardsInCollection = _.find(cardsCollection, cardFromCollection => cardFromCollection.name === cardCopy.name)
     // If it does...
-    if (cardInCollection) {
-      console.log('%c   existing card', 'color: #A1C659;')
+    if (cardsInCollection) {
       // Reassign it as a 'cardCopy'
-      cardCopy = cardInCollection.copy()
+      cardCopy = cardsInCollection.copy()
       // Check if it contains chosen variant
       const variantToUpdate = _.find(cardCopy.variants, { id: variantCopy.id })
       // If it does...
       if (variantToUpdate) {
-        console.log('%c   variant exists', 'color: #A1C659;')
+        console.log('%c   existing card - existing variant - incrementing', 'color: #A1C659;')
         // Increment its count
         variantToUpdate.cardsInCollection++
       // In other case...
       } else {
-        console.log('%c   variant does not exist', 'color: #A1C659;')
+        console.log('%c   existing card - new variant', 'color: #A1C659;')
         // Set variant's count to 1
         variantCopy.cardsInCollection = 1
         // Insert it into main card
@@ -78,7 +75,7 @@ const ACTION_HANDLERS = {
       // Increment main card's count
       cardCopy.cardsInCollection++
       // Get that card's index in the collection
-      const cardIndex = _.findIndex(cardsCollection, cardFromCollection => cardFromCollection.name === card.name)
+      const cardIndex = _.findIndex(cardsCollection, cardFromCollection => cardFromCollection.id === cardCopy.id)
       // Use that index to insert the updated card into the collection
       cardsCollection = [
         ...cardsCollection.slice(0, cardIndex),
@@ -87,7 +84,6 @@ const ACTION_HANDLERS = {
       ]
       // Save changes to Local Storage
       updateLocalStorage(cardsCollection)
-      console.warn('cardsCollection', cardsCollection)
       // Update the store
       return {
         ...state,
@@ -96,7 +92,7 @@ const ACTION_HANDLERS = {
     }
 
     // If chosen card doesn't exist in the collection yet...
-    console.log('%c   new card', 'color: #A1C659;')
+    console.log('%c   new card - new variant', 'color: #A1C659;')
     // Set both its chosen variant's counts to one
     cardCopy.cardsInCollection = 1
     variantCopy.cardsInCollection = 1
@@ -112,40 +108,68 @@ const ACTION_HANDLERS = {
       cards: cardsCollection
     }
   },
-  [REMOVE_CARD]: (state, { payload }) => {
-    console.log('%cREMOVE_CARD payload', 'color: #A1C659;', payload)
-    let cardsArray = []
-    const cardIndex = _.findIndex(state, (objectInState) => objectInState.id === payload.id)
-    const cardFromState = _.find(state, (objectInState) => objectInState.id === payload.id)
+  [REMOVE_CARD]: (state, { card, variant }) => {
+    console.log('%cREMOVE_CARD payload', 'color: #A1C659;', variant.name)
+    // Copy card collection from store
+    let cardsCollection = [...state.cards]
+    // Make copies of both main card and its chosen variant
+    let cardCopy = _.find(cardsCollection, { id: card.id }).copy()
+    const variantCopy = _.find(cardCopy.variants, { id: variant.id }).copy()
+    // Find their respective indexes
+    const cardIndex = _.findIndex(cardsCollection, cardFromCollection => cardFromCollection.id === cardCopy.id)
+    const variantIndex = _.findIndex(cardCopy.variants, cardVariant => cardVariant.id === variantCopy.id)
 
-    if (cardFromState.cardsInMyCards > 1) {
-      // If more then one card
-      const foo = new Card(cardFromState)
-      foo.decrease()
-      cardsArray = [
-        ...state.slice(0, cardIndex),
-        foo,
-        ...state.slice(cardIndex + 1)
+    // If there is only one main card...
+    if (cardCopy.cardsInCollection === 1) {
+      console.log('%c   only one main card - removing it', 'color: #A1C659;')
+      // Remove it from the collection
+      cardsCollection = [
+        ...cardsCollection.slice(0, cardIndex),
+        ...cardsCollection.slice(cardIndex + 1)
       ]
-      updateLocalStorage(cardsArray)
-
-      return {
-        ...state,
-        cards: cardsArray
-      }
     }
 
-    // If only one card
-    cardsArray = [
-      ...state.slice(0, cardIndex),
-      ...state.slice(cardIndex + 1)
-    ]
+    // If there are more copies of the main card...
+    if (cardCopy.cardsInCollection > 1) {
+      // If there is only one card of the chosen variant...
+      if (variantCopy.cardsInCollection === 1) {
+        console.log('%c   only one variant card - removing it', 'color: #A1C659;')
+        // Remove it from the main card
+        cardCopy.variants = [
+          ...cardCopy.variants.slice(0, variantIndex),
+          ...cardCopy.variants.slice(variantIndex + 1)
+        ]
+      }
 
-    updateLocalStorage(cardsArray)
+      // If there are more copies of the chosen variant...
+      if (variantCopy.cardsInCollection > 1) {
+        console.log('%c   more variant cards - decrementing', 'color: #A1C659;')
+        // Decrement it
+        variantCopy.cardsInCollection--
+        // Update main card's varaint array
+        cardCopy.variants = [
+          ...cardCopy.variants.slice(0, variantIndex),
+          variantCopy,
+          ...cardCopy.variants.slice(variantIndex + 1)
+        ]
+      }
 
+      // Decrement the main card
+      cardCopy.cardsInCollection--
+      // Update the cards collection
+      cardsCollection = [
+        ...cardsCollection.slice(0, cardIndex),
+        cardCopy,
+        ...cardsCollection.slice(cardIndex + 1)
+      ]
+    }
+
+    // Save changes to Local Storage
+    updateLocalStorage(cardsCollection)
+    // Update the store
     return {
       ...state,
-      cards: cardsArray
+      cards: cardsCollection
     }
   },
   [CLEAR_MY_CARDS]: (state) => initialState,
