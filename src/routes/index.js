2d3a21b25a663a26a70d0e398a50697da67881f1
Robert Kirsz <robert.kirsz@gmail.com>
2017-02-27 21:17:33 +0100

    Added route authorization

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
+import { auth }      from 'utils/firebase'
 import CoreLayout    from 'layouts/CoreLayout'
 import HomeView      from 'routes/HomeView'
 import AllCardsView  from 'routes/AllCardsView'
@@ -7,7 +8,15 @@ import ProfileView   from 'routes/ProfileView'
 import SettingsView  from 'routes/SettingsView'
 import NotFoundView  from 'routes/NotFoundView'
 
-// TODO: add route names
+// Redirects unathorized users
+const requireAuth = (nextState, replace) => {
+  if (!auth.currentUser) {
+    replace({
+      pathname: '/',
+      state: { nextPathname: nextState.location.pathname }
+    })
+  }
+}
 
 export const createRoutes = store => ({
   path: '/',
@@ -19,20 +28,32 @@ export const createRoutes = store => ({
       component: AllCardsView,
       showAppButtons: true,
       childRoutes: [
-        { path: '/all-cards/:cardUrl', component : CardView }
+        { path: '/all-cards/:cardUrl', component: CardView }
       ]
     },
     {
       path: 'my-cards',
       component: MyCardsView,
       showAppButtons: true,
+      onEnter: requireAuth,
       childRoutes: [
-        { path: '/my-cards/:cardUrl', component : CardView }
+        { path: '/my-cards/:cardUrl', component: CardView }
       ]
     },
-    { path: 'settings', component : SettingsView },
-    { path: 'profile', component : ProfileView },
-    { path: '*', component : NotFoundView }
+    {
+      path: 'settings',
+      component: SettingsView,
+      onEnter: requireAuth
+    },
+    {
+      path: 'profile',
+      component: ProfileView,
+      onEnter: requireAuth
+    },
+    {
+      path: '*',
+      component: NotFoundView
+    }
   ]
 })
 
